c9111151927299b1513f2909203cd75b2f1b8672
Cameron <kramin42@gmail.com>
2014-01-28 22:48:06 +1300

    Added palette editor

--- src/cameron/gpufractal/Main.java
+++ src/cameron/gpufractal/Main.java
@@ -7,8 +7,12 @@ import static org.lwjgl.opengl.GL30.glBindFramebuffer;
 
 import java.awt.Font;
 import java.io.File;
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
 import java.util.Random;
 
+import org.lwjgl.BufferUtils;
 import org.lwjgl.Sys;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
@@ -44,6 +48,18 @@ public class Main{
     Fractal julia;
     boolean viewingMandelbrot = true;
     
+    int palette;
+    int palW = sw, palH = 300;
+    
+    ArrayList<float[]> palR = new ArrayList<float[]>();
+    ArrayList<float[]> palG = new ArrayList<float[]>();
+    ArrayList<float[]> palB = new ArrayList<float[]>();
+    ArrayList<ArrayList<float[]>> pals = new ArrayList<ArrayList<float[]>>();
+    boolean paletteDragging = false;
+    int paletteChannel = 1;
+    int palettePoint = 1;
+    
+    
     Random rand = new Random();
     //julia animation test vars
     //float vx = 0.0f, vy = 0.0f;
@@ -61,6 +77,34 @@ public class Main{
         julia.extraVars[0] = -0.77f;
         julia.extraVars[1] = 0.2f;
         julia.savePrevState();
+        
+        ByteBuffer buf = null;
+	    
+	    palette=glGenTextures();
+	    glBindTexture( GL_TEXTURE_2D, palette );
+	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	    glTexImage2D(GL_TEXTURE_2D, 0,GL_RGBA,palW,palH,0,GL_RGBA,GL_UNSIGNED_INT, buf);
+	    
+	    pals.add(palR);
+	    pals.add(palG);
+	    pals.add(palB);
+	    palR.add(new float[]{0.0f,0.0f});
+	    palR.add(new float[]{0.4f,0.0f});
+	    palR.add(new float[]{0.5f,1.0f});
+	    palR.add(new float[]{1.0f,0.0f});
+	    palG.add(new float[]{0.0f,0.0f});
+	    palG.add(new float[]{0.3f,0.0f});
+	    palG.add(new float[]{0.5f,1.0f});
+	    palG.add(new float[]{0.7f,0.0f});
+	    palG.add(new float[]{1.0f,0.0f});
+	    palB.add(new float[]{0.0f,0.0f});
+	    palB.add(new float[]{0.5f,1.0f});
+	    palB.add(new float[]{0.6f,0.0f});
+	    palB.add(new float[]{1.0f,0.0f});
+	    
+	    calcPalette();
+        
         while(!done){
             if(Display.isCloseRequested())
             	done=true;
@@ -122,6 +166,97 @@ public class Main{
         glUseProgram(0);
         fractal.draw(sw,0.0f,w,h,false);
         small_fractal.draw(0.0f,0.0f,sw,sw*((float) h)/(w-sw),true);
+        
+        //draw the palette (at low alpha)
+        glColor4f(1.0f, 1.0f, 1.0f,0.7f);
+        glEnable(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D, palette);
+        glBegin(GL_QUADS);
+	        glTexCoord2f(0, 1); glVertex3f( 0, sw*((float) h)/(w-sw), -1.0f);
+	        glTexCoord2f(1, 1); glVertex3f( palW, sw*((float) h)/(w-sw), -1.0f);
+	        glTexCoord2f(1, 0); glVertex3f( palW, sw*((float) h)/(w-sw)+palH, -1.0f);
+	        glTexCoord2f(0, 0); glVertex3f( 0, sw*((float) h)/(w-sw)+palH, -1.0f);
+        glEnd();
+        glDisable(GL_TEXTURE_2D);
+        
+        //draw the r,g,b channels
+        glColor4f(0.5f,0.5f,0.5f,0.5f);
+        glBegin(GL_LINE_LOOP);
+        	glVertex3f( 0, sw*((float) h)/(w-sw), -1.0f);
+        	glVertex3f( sw, sw*((float) h)/(w-sw), -1.0f);
+        	glVertex3f( sw, sw*((float) h)/(w-sw)+palH, -1.0f);
+        	glVertex3f( 0, sw*((float) h)/(w-sw)+palH, -1.0f);
+        glEnd();
+        glBegin(GL_LINES);
+        	glVertex3f( 0, sw*((float) h)/(w-sw)+palH/3, -1.0f);
+        	glVertex3f( sw, sw*((float) h)/(w-sw)+palH/3, -1.0f);
+        	glVertex3f( 0, sw*((float) h)/(w-sw)+2*palH/3, -1.0f);
+        	glVertex3f( sw, sw*((float) h)/(w-sw)+2*palH/3, -1.0f);
+        glEnd();
+        //red
+        float offsetY = sw*((float) h)/(w-sw)+2*palH/3;
+        float multX = palW;
+        float multY = palH/3.0f;
+        glColor4f(1.0f,0.0f,0.0f,1.0f);
+        glBegin(GL_LINE_STRIP);
+        for (int i=0; i<palR.size(); i++){
+        	glVertex3f(palR.get(i)[0]*multX,palR.get(i)[1]*multY+offsetY,-1.0f);
+        }
+        glEnd();
+        //green
+        offsetY = sw*((float) h)/(w-sw)+palH/3;
+        glColor4f(0.0f,1.0f,0.0f,1.0f);
+        glBegin(GL_LINE_STRIP);
+        for (int i=0; i<palG.size(); i++){
+        	glVertex3f(palG.get(i)[0]*multX,palG.get(i)[1]*multY+offsetY,-1.0f);
+        }
+        glEnd();
+        //blue
+        offsetY = sw*((float) h)/(w-sw);
+        glColor4f(0.0f,0.0f,1.0f,1.0f);
+        glBegin(GL_LINE_STRIP);
+        for (int i=0; i<palB.size(); i++){
+        	glVertex3f(palB.get(i)[0]*multX,palB.get(i)[1]*multY+offsetY,-1.0f);
+        }
+        glEnd();
+        
+        if (paletteChannel>=0 && palettePoint >= 0){
+        	glEnable( GL_POINT_SMOOTH );
+        	glPointSize(4);
+        	offsetY = sw*((float) h)/(w-sw)+(2-paletteChannel)*palH/3;
+        	float x = 0,y = 0;
+        	switch (paletteChannel){
+        	case 0:
+        		glColor4f(1.0f,0.0f,0.0f,1.0f);
+        		x = palR.get(palettePoint)[0];
+        		y = palR.get(palettePoint)[1];
+        		break;
+        	case 1:
+        		glColor4f(0.0f,1.0f,0.0f,1.0f);
+        		x = palG.get(palettePoint)[0];
+        		y = palG.get(palettePoint)[1];
+        		break;
+        	case 2:
+        		glColor4f(0.0f,0.0f,1.0f,1.0f);
+        		x = palB.get(palettePoint)[0];
+        		y = palB.get(palettePoint)[1];
+        		break;
+        	}
+        	glBegin(GL_POINTS);
+        	glVertex3f(x*multX,y*multY+offsetY,-1.0f);
+        	glEnd();
+        	if (paletteDragging){
+        		x = pals.get(paletteChannel).get(palettePoint)[0]*(palW);
+        		y = pals.get(paletteChannel).get(palettePoint)[1]*(palH/3.0f) + sw*((float) h)/(w-sw) + (2-paletteChannel)/3.0f*palH;
+        		glBegin(GL_LINES);
+	        		glVertex3f(x,sw*((float) h)/(w-sw),-1.0f);
+	        		glVertex3f(x,sw*((float) h)/(w-sw)+palH,-1.0f);
+	        		glVertex3f(0,y,-1.0f);
+	        		glVertex3f(palW,y,-1.0f);
+        		glEnd();
+        	}
+        }
+        
         
         //draw cross-hair for the Julia fractal's complex parameter on the Mandelbrot
         if (viewingMandelbrot){
@@ -208,7 +343,39 @@ public class Main{
     	}
     	int mdx = Mouse.getDX();
     	int mdy = Mouse.getDY();
-		if (Mouse.isButtonDown(0) && Mouse.getX()>sw && !Keyboard.isKeyDown(Keyboard.KEY_LCONTROL) && !mouseDragging && (mdx!=0 || mdy!=0)){
+    	double modifier = 1.0;
+		if (Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)) modifier*=0.1;
+		if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) modifier*=10.0;
+    	//System.out.println(Mouse.getX()+","+Mouse.getY());
+    	if (!paletteDragging){
+	    	if (Mouse.getX()>0 && Mouse.getX()<palW && Mouse.getY()>sw*((float) h)/(w-sw) && Mouse.getY()<sw*((float) h)/(w-sw)+palH){
+	    		//System.out.println(Mouse.getX()+","+Mouse.getY());
+	    		paletteChannel = 2 - (int)(3*(Mouse.getY() - sw*((float) h)/(w-sw)))/(palH);
+	    		//System.out.println(paletteChannel);
+	    		float mindistsq = 100000000;
+	    		palettePoint = -1;
+	    		float x = Mouse.getX()/(float)palW;
+	    		float y = 3*(Mouse.getY() - sw*((float) h)/(w-sw) - (2-paletteChannel)/3.0f*palH)/(float)palH;
+	    		//System.out.println(y);
+	    		for (int i=0; i<pals.get(paletteChannel).size(); i++){
+	    			float distsq = (pals.get(paletteChannel).get(i)[0]-x)*(pals.get(paletteChannel).get(i)[0]-x) + (pals.get(paletteChannel).get(i)[1]-y)*(pals.get(paletteChannel).get(i)[1]-y);
+	    			if (distsq<mindistsq){
+	    				mindistsq = distsq;
+	    				palettePoint = i;
+	    			}
+	    		}
+	    	} else {
+	    		paletteChannel = -1;
+	    	}
+    	} else {
+    		if (palettePoint != 0 && palettePoint != pals.get(paletteChannel).size()-1){
+	    		pals.get(paletteChannel).get(palettePoint)[0]+=modifier*mdx/(float)palW;
+	    		pals.get(paletteChannel).get(palettePoint)[0] = Math.max(pals.get(paletteChannel).get(palettePoint-1)[0], Math.min(pals.get(paletteChannel).get(palettePoint+1)[0], pals.get(paletteChannel).get(palettePoint)[0]));//clamp
+    		}
+    		pals.get(paletteChannel).get(palettePoint)[1]+=modifier*mdy*3.0f/palH;
+    		pals.get(paletteChannel).get(palettePoint)[1] = Math.max(0, Math.min(1, pals.get(paletteChannel).get(palettePoint)[1]));//clamp
+    	}
+		if (!paletteDragging && Mouse.isButtonDown(0) && Mouse.getX()>sw && !Keyboard.isKeyDown(Keyboard.KEY_LCONTROL) && !mouseDragging && (mdx!=0 || mdy!=0)){
 			//System.out.println("mdx: "+mdx+", mdy: "+mdy);
 			fractal.move((float) (-mdx/(h*0.5*fractal.zoom)),(float) (-mdy/(h*0.5*fractal.zoom)));
 		}
@@ -246,11 +413,21 @@ public class Main{
     				}
 					mouseDragging = false;
     			}
-    		} else if (Mouse.getEventButton() == 0 && Mouse.getEventButtonState()){//LMB
-    			if (Mouse.getX() >= sw && Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)){
-    				fractal.savePrevState();
-    				fractal.move((float) (-(sw/2 + w/2 - Mouse.getX())/((w-sw)*(h/(float)(w-sw))*0.5*fractal.zoom)),(float) (-(h/2 - Mouse.getY())/(h*0.5*fractal.zoom)));
-    				Mouse.setCursorPosition(sw/2 + w/2, h/2);
+    		} else if (Mouse.getEventButton() == 0){//LMB
+    			if (Mouse.getEventButtonState()){//push
+	    			if (Mouse.getX() >= sw && Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)){
+	    				fractal.savePrevState();
+	    				fractal.move((float) (-(sw/2 + w/2 - Mouse.getX())/((w-sw)*(h/(float)(w-sw))*0.5*fractal.zoom)),(float) (-(h/2 - Mouse.getY())/(h*0.5*fractal.zoom)));
+	    				Mouse.setCursorPosition(sw/2 + w/2, h/2);
+	    			}
+	    			if (Mouse.getX()>0 && Mouse.getX()<palW && Mouse.getY()>sw*((float) h)/(w-sw) && Mouse.getY()<sw*((float) h)/(w-sw)+palH){
+	    				paletteDragging = true;
+	    			}
+    			} else {//release
+    				if (paletteDragging){
+    					paletteDragging = false;
+    					calcPalette();
+    				}
     			}
     		} else {
     			int dwheel = Mouse.getEventDWheel();
@@ -260,9 +437,6 @@ public class Main{
     	
     	while (Keyboard.next()){
     		if (Keyboard.getEventKeyState()){
-    			double modifier = 1.0;
-    			if (Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)) modifier*=0.1;
-    			if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) modifier*=10.0;
     			switch(Keyboard.getEventKey()){
     			case Keyboard.KEY_RETURN:
     				fractal.savePrevState();
@@ -347,6 +521,44 @@ public class Main{
         catch (SlickException e) { 
            e.printStackTrace(); 
         }
+    }
+    
+    void calcPalette(){
+	    for (int i=0; i<palW; i++){
+	    	for (int j=0; j<palH; j++){
+	    		float mu = (i+j*palW)/(float)(palW*palH);
+		    	//changeTexelColor(palette, i, j, mu, mu, mu, 1.0f);
+	    		float[] cvals = {0.0f,0.0f,0.0f,1.0f};
+	    		for (int n=0; n<3; n++){
+	    			//get the two surrounding points
+	    			int pointA = 0, pointB = 0;
+	    			while (pals.get(n).get(pointB)[0]<=mu){
+	    				pointB++;
+	    			}
+	    			pointA = pointB - 1;
+	    			//then interpolate
+	    			cvals[n] = pals.get(n).get(pointA)[1] + (mu-pals.get(n).get(pointA)[0])*((pals.get(n).get(pointB)[1]-pals.get(n).get(pointA)[1])/(pals.get(n).get(pointB)[0]-pals.get(n).get(pointA)[0]));
+	    		}
+	    		changeTexelColor(palette, i, j, cvals[0], cvals[1], cvals[2], cvals[3]);
+		    }
+	    }
+    }
+    
+    void changeTexelColor(int id, int x, int y, float r, float g, float b, float a) {
+        FloatBuffer data = BufferUtils.createFloatBuffer(4);
+        float[] d = {r,g,b,a};
+        data.put(d);
+        data.flip();
+        glBindTexture(GL_TEXTURE_2D, id);
+        glTexSubImage2D(GL_TEXTURE_2D,
+                        0,
+                        x,
+                        y,
+                        1,
+                        1,
+                        GL_RGBA,
+                        GL_FLOAT,
+                        data);
     }
     
     /**
